4d1a3ec4f753040e5104d444131b4d62ce7c62d0
David Baetge <david.baetge@gmail.com>
2022-05-05 02:18:35 +0200

    Added icons for all used observations. Added diagram-tile and react entry.

--- bin/user/get_icon.py
+++ bin/user/get_icon.py
@@ -14,11 +14,58 @@ class GetIcon(SearchList):
         Returns:
             str: A color string
         """
-
         icon_path = "includes/icons/"
-        if observation == 'outTemp':
+
+        if observation == 'outTemp' or observation == 'inTemp':
             return icon_path + 'temp.svg'
-        elif observation == 'outHumidity':
+
+        elif observation == 'outHumidity' or observation == 'inHumidity':
             return icon_path + 'humidity.svg'
+
         elif observation == 'barometer':
             return icon_path + 'barometer.svg'
+
+        elif observation == 'windSpeed':
+            return icon_path + 'wind-speed.svg'
+
+        elif observation == 'windGust':
+            return icon_path + 'wind-gust.svg'
+
+        elif observation == 'windDir':
+            return icon_path + 'wind-direction.svg'
+
+        elif observation == 'rain':
+            return icon_path + 'rain.svg'
+
+        elif observation == 'rainRate':
+            return icon_path + 'rain-rate.svg'
+
+        elif observation == 'dewpoint':
+            return icon_path + 'dew-point.svg'
+
+        elif observation == 'windchill':
+            return icon_path + 'wind-chill.svg'
+
+        elif observation == 'heatindex':
+            return icon_path + 'heat.svg'
+
+        elif observation == 'UV':
+            return icon_path + 'uv.svg'
+
+        elif observation == 'ET':
+            return icon_path + 'ev.svg'
+
+        elif observation == 'radiation':
+            return icon_path + 'solar.svg'
+
+        elif observation == 'appTemp':
+            return icon_path + 'feel-temp.svg'
+
+        elif observation == 'cloudbase':
+            return icon_path + 'cloud-base.svg'
+
+        elif 'Temp' in observation:
+            return icon_path + 'temp.svg'
+
+        elif 'Humid' in observation:
+            return icon_path + 'humidity.svg'

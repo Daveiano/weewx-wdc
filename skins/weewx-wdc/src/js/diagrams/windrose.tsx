5c3beecb4998bdfc04febfa710b0248444148b09
David Baetge <david.baetge@gmail.com>
2022-07-27 01:57:09 +0200

    Updated windrose tooltip.

--- skins/weewx-wdc/src/js/diagrams/windrose.tsx
+++ skins/weewx-wdc/src/js/diagrams/windrose.tsx
@@ -1,6 +1,8 @@
-import { PropTypes } from "carbon-components-react";
 import React, { FunctionComponent, useRef, useState } from "react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import createPlotlyComponent from "react-plotly.js/factory";
+import { Maximize } from "../assets/maximize";
+
 import { TooltipWindrose } from "../components/tooltip-windrose";
 import { WindRoseProps } from "./types";
 
@@ -11,104 +13,121 @@ export const WindRoseDiagram: FunctionComponent<WindRoseProps> = (
   props: WindRoseProps
 ): React.ReactElement => {
   const hoverInfoRef = useRef<HTMLDivElement>(null);
+  const hoverInfoRefFullscreen = useRef<HTMLDivElement>(null);
   const [title, setTitle] = useState("");
   const [color, setColor] = useState("");
   const [template, setTemplate] = useState("");
   const [replacements, setReplacements] = useState({});
+  const [hoverInfoPosition, setHoverInfoPosition] = useState({
+    top: 0,
+    left: 0,
+  });
+  const handle = useFullScreenHandle();
+
+  const windroseDiagram = (
+    <Plot
+      data={props.data}
+      layout={{
+        hovermode: "closest",
+        dragmode: false,
+        font: { size: 12 },
+        legend: {
+          font: { size: 12 },
+          xanchor: "left",
+          //orientation: "v",
+          x: -0.5,
+          y: 0.5,
+        },
+        autosize: true,
+        margin: {
+          b: 30,
+          l: 0,
+          pad: 0,
+          r: 0,
+          t: 30,
+        },
+        barmode: "stack",
+        bargap: 0,
+        polar: {
+          //barmode: "stack",
+          //bargap: 0,
+          radialaxis: {
+            ticksuffix: "%",
+            angle: 45,
+            dtick: 20,
+          },
+          angularaxis: { direction: "clockwise" },
+        },
+        colorway: colors,
+        yaxis: {
+          range: [0, 20],
+        },
+      }}
+      config={{
+        responsive: true,
+        displayModeBar: false,
+        showAxisDragHandles: false,
+      }}
+      style={{ width: "100%", height: "100%" }}
+      useResizeHandler={true}
+      onBeforeHover={(event) => {
+        if ((event as any).type === "mousemove") {
+          setHoverInfoPosition({
+            top: (event as any).layerY,
+            left: (event as any).layerX,
+          });
+        }
+
+        return true;
+      }}
+      onHover={(data) => {
+        let hoverInfo = hoverInfoRef.current;
+
+        if (handle.active) {
+          hoverInfo = hoverInfoRefFullscreen.current;
+        }
+
+        if (hoverInfo) {
+          setTitle((data.points[0] as any).fullData.name);
+          setColor((data.points[0] as any).fullData.marker.color);
+          setTemplate((data.points[0] as any).fullData.hovertemplate);
+          setReplacements({
+            r: (data.points[0] as any).r,
+            theta: (data.points[0] as any).theta,
+          });
+
+          hoverInfo.style.display = "block";
+        }
+      }}
+      onUnhover={() => {
+        let hoverInfo = hoverInfoRef.current;
+
+        if (handle.active) {
+          hoverInfo = hoverInfoRefFullscreen.current;
+        }
+
+        if (hoverInfo) {
+          hoverInfo.style.display = "none";
+        }
+      }}
+    />
+  );
+
+  console.log(hoverInfoPosition);
 
   return (
     <>
-      <Plot
-        data={props.data}
-        layout={{
-          hovermode: "closest",
-          dragmode: false,
-          font: { size: 12 },
-          legend: {
-            font: { size: 12 },
-            xanchor: "left",
-            //orientation: "v",
-            x: -0.5,
-            y: 0.5,
-          },
-          autosize: true,
-          margin: {
-            b: 30,
-            l: 0,
-            pad: 0,
-            r: 0,
-            t: 30,
-          },
-          barmode: "stack",
-          bargap: 0,
-          polar: {
-            //barmode: "stack",
-            //bargap: 0,
-            radialaxis: {
-              ticksuffix: "%",
-              angle: 45,
-              dtick: 20,
-            },
-            angularaxis: { direction: "clockwise" },
-          },
-          colorway: colors,
-          yaxis: {
-            range: [0, 20],
-          },
+      <Maximize onClick={handle.enter} />
+      <div className="diagram">{windroseDiagram}</div>
+      <div
+        id="hoverInfoWindRose"
+        ref={hoverInfoRef}
+        className="windrose-tooltip"
+        style={{
+          left: `${hoverInfoPosition.left + 25}px`,
+          top: `${hoverInfoPosition.top - 10}px`,
         }}
-        config={{
-          responsive: true,
-          displayModeBar: false,
-          showAxisDragHandles: false,
-        }}
-        style={{ width: "100%", height: "100%" }}
-        useResizeHandler={true}
-        onHover={(data) => {
-          console.log(data);
-          console.log(
-            (data.points[0].xaxis as any).l2p((data.points[0] as any).theta)
-          );
-          console.log((data.points[0].yaxis as any)._offset);
-
-          console.log(
-            parseFloat(
-              (data.points[0].yaxis as any).l2p((data.points[0] as any).theta)
-            ) + parseFloat((data.points[0].yaxis as any)._offset)
-          );
-
-          const hoverInfo = hoverInfoRef.current;
-
-          if (hoverInfo) {
-            setTitle((data.points[0] as any).fullData.name);
-            setColor((data.points[0] as any).fullData.marker.color);
-            setTemplate((data.points[0] as any).fullData.hovertemplate);
-            setReplacements({
-              r: (data.points[0] as any).r,
-              theta: (data.points[0] as any).theta,
-            });
-
-            hoverInfo.style.left = `${
-              (data.points[0].xaxis as any).l2p((data.points[0] as any).r) +
-              (data.points[0].xaxis as any)._offset
-            }px`;
-
-            hoverInfo.style.top = `${
-              (data.points[0].xaxis as any).l2p((data.points[0] as any).r) +
-              (data.points[0].yaxis as any)._offset
-            }px`;
-
-            hoverInfo.style.display = "block";
-          }
-        }}
-        onUnhover={() => {
-          const hoverInfo = hoverInfoRef.current;
-
-          if (hoverInfo) {
-            //hoverInfo.style.display = "none";
-          }
-        }}
-      />
-      <div id="hoverInfoWindRose" ref={hoverInfoRef}>
+      >
         <TooltipWindrose
           color={color}
           template={template}
@@ -116,6 +135,26 @@ export const WindRoseDiagram: FunctionComponent<WindRoseProps> = (
           title={title}
         />
       </div>
+      <FullScreen handle={handle}>
+        <Maximize onClick={handle.exit} />
+        {windroseDiagram}
+        <div
+          id="hoverInfoWindRose"
+          ref={hoverInfoRefFullscreen}
+          className="windrose-tooltip"
+          style={{
+            left: `${hoverInfoPosition.left + 60}px`,
+            top: `${hoverInfoPosition.top + 25}px`,
+          }}
+        >
+          <TooltipWindrose
+            color={color}
+            template={template}
+            replacements={replacements}
+            title={title}
+          />
+        </div>
+      </FullScreen>
     </>
   );
 };

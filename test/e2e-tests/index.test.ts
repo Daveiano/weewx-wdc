ca404b6dd54b92a5c0ea1637f6bb35945fa867dc
David Baetge <david.baetge@gmail.com>
2022-08-10 01:21:44 +0200

    Added basic jest testing tesup with jsom. [GH-34]

---
+++ test/e2e-tests/index.test.ts
@@ -0,0 +1,30 @@
+import { fireEvent, getByText } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+import { JSDOM } from "jsdom";
+import fs from "fs";
+import path from "path";
+
+const html = fs.readFileSync(
+  path.resolve(__dirname, "weewx/index.html"),
+  "utf8"
+);
+
+let dom;
+let container: HTMLElement;
+
+describe("index.html", () => {
+  beforeEach(() => {
+    dom = new JSDOM(html, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  test("Basic Display", () => {
+    expect(container.querySelector("h1")).not.toBeNull();
+    expect(container.querySelector("h1")).toHaveTextContent(
+      "Current Weather Conditions"
+    );
+    // expect(
+    //   getByText(container, "Current Weather Conditions")
+    // ).toBeInTheDocument();
+  });
+});

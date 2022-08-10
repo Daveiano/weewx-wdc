ce47cf4f5517355335496727065066eb34968506
David Baetge <david.baetge@gmail.com>
2022-08-10 23:29:21 +0200

    Removed jest, testing-lib and jsom. Added playwright tests. [GH-34]

--- test/e2e-tests/index.test.ts
+++ test/e2e-tests/index.test.ts
@@ -1,33 +1,20 @@
-import { fireEvent, getByText } from "@testing-library/dom";
-import "@testing-library/jest-dom/extend-expect";
-import { JSDOM } from "jsdom";
-import fs from "fs";
-import path from "path";
+import { test, expect, type Page } from "@playwright/test";
 
-const html = fs.readFileSync(
-  path.resolve(
-    __dirname,
-    "artifacts-alternative-weewx-html/public_html/index.html"
-  ),
-  "utf8"
-);
-
-let dom;
-let container: HTMLElement;
-
-describe("index.html", () => {
-  beforeEach(() => {
-    dom = new JSDOM(html, { runScripts: "dangerously" });
-    container = dom.window.document.body;
+test.describe("Alternative index.html", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("artifacts-alternative-weewx-html/public_html/index.html");
   });
 
-  test("Basic Display", () => {
-    expect(container.querySelector("h1")).not.toBeNull();
-    expect(container.querySelector("h1")).toHaveTextContent(
-      "Current Weather Conditions"
-    );
+  test("Basic Display", async ({ page }) => {
+    // expect(container.querySelector("h1")).not.toBeNull();
+    // expect(container.querySelector("h1")).toHaveTextContent(
+    //   "Current Weather Conditions"
+    // );
     // expect(
-    //   getByText(container, "Current Weather Conditions")
+    //   getByText(container, "Haselbachtal, Saxony, Germany")
     // ).toBeInTheDocument();
+    // const sideNav = getByLabelText(container, "Side navigation");
+    // expect(getAllByRole(sideNav, "listitem", { hidden: true }).length).toBe(10);
+    await expect(page.locator("h1")).toHaveText("Current Weather Conditions");
   });
 });

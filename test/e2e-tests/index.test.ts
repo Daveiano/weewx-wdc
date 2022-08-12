a667f6fa49637f67f09b62c4e3658c8385513083
David Baetge <david.baetge@gmail.com>
2022-08-12 02:14:36 +0200

    Added more tests. [GH-34]

--- test/e2e-tests/index.test.ts
+++ test/e2e-tests/index.test.ts
@@ -5,19 +5,59 @@ test.describe("Alternative index.html", () => {
     await page.goto("artifacts-alternative-weewx-html/public_html/index.html");
   });
 
+  test("SideNav and Header", async ({ page }) => {
+    await expect(
+      page.locator("text=WeeWX Haselbachtal, Saxony, Germany")
+    ).toBeVisible();
+
+    // 4 Links: Celestial, Stats, About and Archive & NOAA.
+    await expect(
+      page.locator(
+        "bx-side-nav bx-side-nav-items bx-side-nav-link >> visible=true"
+      )
+    ).toHaveCount(4);
+
+    // 3 Sub-menus: Years, 2021, 2022.
+    await expect(
+      page.locator(
+        "bx-side-nav bx-side-nav-items bx-side-nav-menu >> visible=true"
+      )
+    ).toHaveCount(3);
+
+    // Years menu.
+    await expect(
+      page.locator(
+        "bx-side-nav bx-side-nav-items bx-side-nav-menu[title='Years'] bx-side-nav-menu-item"
+      )
+    ).toHaveCount(2);
+
+    // 2021 menu.
+    await expect(
+      page.locator(
+        "bx-side-nav bx-side-nav-items bx-side-nav-menu[title='2021'] bx-side-nav-menu-item"
+      )
+    ).toHaveCount(10);
+
+    // 2022 menu.
+    await expect(
+      page.locator(
+        "bx-side-nav bx-side-nav-items bx-side-nav-menu[title='2022'] bx-side-nav-menu-item"
+      )
+    ).toHaveCount(6);
+  });
+
+  test("Footer", async ({ page }) => {});
+
   test("Basic Display", async ({ page }) => {
-    // expect(container.querySelector("h1")).not.toBeNull();
-    // expect(container.querySelector("h1")).toHaveTextContent(
-    //   "Current Weather Conditions"
-    // );
-    // expect(
-    //   getByText(container, "Haselbachtal, Saxony, Germany")
-    // ).toBeInTheDocument();
-    // const sideNav = getByLabelText(container, "Side navigation");
-    // expect(getAllByRole(sideNav, "listitem", { hidden: true }).length).toBe(10);
     await expect(page.locator("h1")).toHaveText("Current Weather Conditions");
 
     await expect(page).toHaveScreenshot({ fullPage: true });
+  });
+
+  test("Stat tiles", async ({ page }) => {
+    await expect(
+      page.locator(".stat-tile[data-test='outTemp'] .value span")
+    ).toHaveText("20.9°C");
   });
 });
 

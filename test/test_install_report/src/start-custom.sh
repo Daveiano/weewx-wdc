9173f031e102d2fc1c3445dcb57fb77b36a2ce94
David Baetge <david.baetge@gmail.com>
2022-11-15 22:33:48 +0100

    Updated sleep interval on forecast generate run.

--- test/test_install_report/src/start-custom.sh
+++ test/test_install_report/src/start-custom.sh
@@ -24,8 +24,8 @@ echo "Starting weewx"
 "${WEEWX_HOME}"/bin/weewxd "${WEEWX_HOME}"/weewx.conf --daemon
 
 echo "$CURRENT_DATE"
-echo "Sleeping 140 seconds..."
-sleep 140
+echo "Sleeping 180 seconds..."
+sleep 180
 
 #printf -v DATE_NOW '%(%Y-%m-%d %H:%M:%S)T\n' -1
 
